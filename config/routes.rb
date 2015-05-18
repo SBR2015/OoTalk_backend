d2b215a955c35b02111cc761685917245c1054c5
Yusuke Ohashi <yusuke@junkpiano.me>
2015-05-18 17:44:48 +0900

    add versioning to api.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :demos, except: [:new, :edit]
+  namespace :api do
+    namespace :v1 do
+      resources :demo
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
