756205ec1d99b2e8a193076d9d6d4ac57c1f82f2
pyar6329 <pyar6329@gmail.com>
2015-06-13 17:51:37 +0900

    add test code from  abstract syntax lists

--- spec/controllers/api/v1/abstract_syntax_controller_spec.rb
+++ spec/controllers/api/v1/abstract_syntax_controller_spec.rb
@@ -7,6 +7,18 @@ RSpec.describe Api::V1::AbstractSyntaxController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+  end
+
+  describe "GET #show" do
+    it "should be able to success" do
+      expect(get :show, {language: :ja}).to have_http_status(:success)
+    end
+
+    it "shouldn't be able to success" do
+      expect{get :show, {language: :ar}}.to raise_error(I18n::InvalidLocale)
+    end
+
   end
 
 end
