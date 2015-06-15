adc98fc535ba78e1b7be8b81a2eda06611b17f4e
pyar6329 <pyar6329@gmail.com>
2015-06-15 21:05:27 +0900

    add codelimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require "codeclimate-test-reporter"
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -86,4 +88,5 @@ RSpec.configure do |config|
 =end
 
   config.color = true
+  CodeClimate::TestReporter.start unless ENV['CODECLIMATE_REPO_TOKEN'] == nil
 end
