e43bd823a212ef9627791872857869b84a959c1f
Yusuke Ohashi <github@junkpiano.me>
2015-11-21 00:31:43 +0900

    fixed a test case
    - for wrong parameter when getting abstract syntax tree

--- spec/controllers/api/v1/abstract_syntax_controller_spec.rb
+++ spec/controllers/api/v1/abstract_syntax_controller_spec.rb
@@ -5,12 +5,8 @@ RSpec.describe Api::V1::AbstractSyntaxController, type: :controller do
   describe "GET #show" do
     it "should be able to success" do
       expect(get :show, {language: :ja}).to have_http_status(:success)
+      expect(get :show, {language: :ar}).to have_http_status(:success)
     end
-
-    it "shouldn't be able to success" do
-      expect{get :show, {language: :ar}}.to raise_error(I18n::InvalidLocale)
-    end
-
   end
 
 end
