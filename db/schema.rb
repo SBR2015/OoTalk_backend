2c26125132c950baeed3a6db2d155ffc809d58bd
Yusuke Ohashi <github@junkpiano.me>
2015-11-29 21:39:30 +0900

    fixed migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128095936) do
+ActiveRecord::Schema.define(version: 20151129123615) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
@@ -49,13 +49,13 @@ ActiveRecord::Schema.define(version: 20151128095936) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
