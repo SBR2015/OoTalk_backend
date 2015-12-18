3282ece8d9a9cb45691fbabfff67520f7984f86a
Yusuke Ohashi <github@junkpiano.me>
2015-12-19 00:34:26 +0900

    update schema

--- db/migrate/20151217134655_devise_token_auth_create_users.rb
+++ db/migrate/20151217134655_devise_token_auth_create_users.rb
@@ -1,11 +1,10 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
   def change
-    add_column :users, :uid, :string, :default => ""
-    add_column :users, :provider, :string, :default => "email"
+    add_column :users, :uid, :string, :null => false, :default => ""
+    add_column :users, :provider, :string, :null => false, :default => "email"
     add_column :users, :name, :string
     add_column :users, :nickname, :string
     add_column :users, :image, :string
     add_column :users, :tokens, :text
-    add_index :users, [:uid, :provider],     :unique => true
   end
 end
