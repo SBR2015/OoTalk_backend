498369f4c98a72d3e7913b073ab44a7db3006d67
Yusuke Ohashi <github@junkpiano.me>
2015-12-19 00:28:32 +0900

    removed indexes.

--- db/migrate/20151217134655_devise_token_auth_create_users.rb
+++ db/migrate/20151217134655_devise_token_auth_create_users.rb
@@ -1,7 +1,7 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration
   def change
-    add_column :users, :uid, :string, :null => false, :default => ""
-    add_column :users, :provider, :string, :null => false, :default => "email"
+    add_column :users, :uid, :string, :default => ""
+    add_column :users, :provider, :string, :default => "email"
     add_column :users, :name, :string
     add_column :users, :nickname, :string
     add_column :users, :image, :string
