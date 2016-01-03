4938d25eae48092c20b1f9ccc797bbfb3c63a3bc
Yusuke Ohashi <github@junkpiano.me>
2016-01-03 17:19:11 +0900

    refactoring coffee scripts.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,8 @@
 //= require ootalk.min.js
 //= require codemirror.js
 //= require mode/javascript/javascript.js
-//= require_tree .
+//= require_tree ./code
+//= require_tree ./course
 // This is optional (in case you have `I18n is not defined` error)
 // If you want to put this line, you must put it BEFORE `i18n/translations`
 //= require i18n
@@ -22,3 +23,6 @@
 //
 // This is a must
 //= require i18n/translations
+//= require_tree ./site-i18n
+//= require code
+//= require staticsite
---
+++ app/assets/javascripts/site-i18n/site_i18n.coffee
@@ -0,0 +1,10 @@
+window.sitei18n =
+    init: ->
+        # I18n    
+        $('#Reset').attr('value',I18n.t('Reset'))
+        $('#Submit').attr('value',I18n.t('Submit'))
+        $('#Code').text(I18n.t('Code'))
+        $('#Input_JSON').text(I18n.t('Input JSON'))
+        $('#Courses').text(I18n.t('Courses'))
+        $('#Language').text(I18n.t('Language'))
+        $('#Menu').text(I18n.t('Menu'))
